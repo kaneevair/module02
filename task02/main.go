b959efb441ae08050f14cea1e5b052e1feae9a29
Ilmira <IKaneeva@mtt.ru>
2024-08-04 14:04:35 +0300

    1task

--- task02/main.go
+++ task02/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	workWeek := []string{"понедельник", "вторник", "среда", "четверг", "пятница"}
 	weekend := []string{"суббота", "воскресенье"}
-
 	var week = append(workWeek, weekend...)
+
 	fmt.Print(week)
 }
